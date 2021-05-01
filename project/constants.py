6e6bae92dc5bcdc4e81ee00355d41cb8057e8d56
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-05-01 20:07:03 +0200

    💼 WebLogs working based on a websocket server

--- project/constants.py
+++ project/constants.py
@@ -1,6 +1,18 @@
 # Libs
 import os
 
+# ---- SERVER ADDRESSES ----
+
+# GUI Server
+GUI_SERVER_PORT = 5000
+GUI_SERVER_ADDR = "localhost"
+GUI_SERVER_ADDR_FULL = f"{GUI_SERVER_ADDR}:{int(GUI_SERVER_PORT)}"
+
+# WebLogger Server
+WEBLOGGER_SERVER_PORT = 5002
+WEBLOGGER_SERVER_ADDR = "localhost"
+WEBLOGGER_SERVER_ADDR_FULL = f"{WEBLOGGER_SERVER_ADDR}:{int(WEBLOGGER_SERVER_PORT)}"
+
 # ---- MAIN ----
 DATA_FOLDER_PATH = os.path.abspath("../data/")
 
