0b3e3e14322e207996db64954bed1b840429ddc1
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-05-02 04:25:21 +0200

    💼 Singleton instead of raw varialbles So the dynamic generation is improved.

--- project/constants.py
+++ project/constants.py
@@ -1,26 +1,38 @@
 # Libs
 import os
 
-# ---- SERVER ADDRESSES ----
+class Constants(object):
+    __instance = None                                                                       # Instance shared varaible
 
-# GUI Server
-GUI_SERVER_PORT = 5000
-GUI_SERVER_ADDR = "localhost"
-GUI_SERVER_ADDR_FULL = f"{GUI_SERVER_ADDR}:{int(GUI_SERVER_PORT)}"
+    #Singleton
+    def __new__(cls, *args, **kwargs):                                                      # Checking upon initialization that the class 
+        if not Constants.__instance:                                                        # hasn't already been initialized
+            Constants.__instance = object.__new__(cls)                                      # if so returning the already
+        return Constants.__instance                                                         # initialized instance..
 
-# WebLogger Server
-WEBLOGGER_SERVER_PORT = 5002
-WEBLOGGER_SERVER_ADDR = "localhost"
-WEBLOGGER_SERVER_ADDR_FULL = f"{WEBLOGGER_SERVER_ADDR}:{int(WEBLOGGER_SERVER_PORT)}"
+    def __init__(self):
+        # ---- SERVER ADDRESSES ----
 
-# ---- MAIN ----
-DATA_FOLDER_PATH = os.path.abspath("../data/")
+        # GUI Server
+        self.GUI_SERVER_PORT = 5000
+        self.GUI_SERVER_ADDR = "localhost"
+        self.GUI_SERVER_ADDR_FULL = f"{self.GUI_SERVER_ADDR}:{int(self.GUI_SERVER_PORT)}"
 
-# ---- VISION ----
+        # WebLogger Server
+        self.WEBLOGGER_SERVER_PORT = 5002
+        self.WEBLOGGER_SERVER_ADDR = "localhost"
+        self.WEBLOGGER_SERVER_ADDR_FULL = f"{self.WEBLOGGER_SERVER_ADDR}:{int(self.WEBLOGGER_SERVER_PORT)}"
 
-# Default device to fall back if settingHandler fails.
-DEFAULT_DEVICE_ID = 0
+        # ---- MAIN ----
+        self.DATA_FOLDER_PATH = os.path.abspath("../data/")
+        self.ENV_DEBUG = True
 
 
-# ---- WEBLOGGER ----
-LOG_STORE_PATH = os.path.abspath("../data/logs/")
+        # ---- VISION ----
+
+        # Default device to fall back if settingHandler fails.
+        self.DEFAULT_DEVICE_ID = 0
+
+
+        # ---- WEBLOGGER ----
+        self.LOG_STORE_PATH = os.path.abspath("../data/logs/")
